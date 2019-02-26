e90dc1ed5dcb9a98fd21b41449d82fb051c77505
andreynho2006 <andreynho2006@gmail.com>
2019-02-26 15:55:11 +0200

    make an initial state for ALbumList

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-//import axios from 'axios';
+import axios from 'axios';
 
 //class Component   (life cicle methods)
 class AlbumList extends Component {
-    // componentWillMount() {
-    //     axios.get('https://rallycoding.herokuapp.com/api/music-albums')
-    //         .then(response => console.log(response))
-    //         .catch(error => console.log(error));
-    // }
+    state = {};
+    
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music-albums')
+            .then(response => console.log(response));
+    }
     render() {
         return (
             <View>
