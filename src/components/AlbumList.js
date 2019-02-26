428fea46032d04e79048908577ef8feb273544ba
andreynho2006 <andreynho2006@gmail.com>
2019-02-26 15:36:31 +0200

    add axios for get the data from an API

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+//import axios from 'axios';
 
-//class Component
+//class Component   (life cicle methods)
 class AlbumList extends Component {
+    // componentWillMount() {
+    //     axios.get('https://rallycoding.herokuapp.com/api/music-albums')
+    //         .then(response => console.log(response))
+    //         .catch(error => console.log(error));
+    // }
     render() {
         return (
             <View>
