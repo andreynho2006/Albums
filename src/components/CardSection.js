3c4837dc8643e580974a9614ac9220099fc68390
andreynho2006 <andreynho2006@gmail.com>
2019-02-28 11:10:25 +0200

    style the CardSection container

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,10 +1,22 @@
-import react from 'react';
+import React from 'react';
 import { View } from 'react-native';
 
 const CardItem = () => {
     return (
-        <View></View>
+        <View style={styles.containerStyle}></View>
     );
 };
+
+const styles = {
+    containerStyle: {
+        borderBottomWidth: 1,
+        padding: 5,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderBottom: '#ddd',
+        position: 'relative',
+    }
+}
 
 export default CardItem;
