2b6b409c52f7f3a97df8dee75d3614f9b3c1f8b0
andreynho2006 <andreynho2006@gmail.com>
2019-02-28 11:36:12 +0200

    add details to the album object

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -7,10 +7,27 @@ const AlbumDetail = (props) => {
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View style={styles.imageView}>
+                    <Image src={props.album.image} />
+                </View>               
+                <View style={styles.headerContentStyles}>
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text> 
+                </View>           
             </CardSection>
         </Card>
     );
 };
+
+const styles = {
+    imageView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+    },
+    headerContentStyles: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    }
+}
 
 export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,11 +5,45 @@ import AlbumDetail from './AlbumDetail';
 
 //class Component   (life cicle methods)
 class AlbumList extends Component {
-    state = { albums: [{ title: 'Taylor Swift' },
-                       { title: 'Fearless' },
-                       { title: 'Speak now' }, 
-                       { title: 'Red' }, 
-                       { title: '1989' }] };
+    state = { 
+        albums: [
+            { 
+                title: 'Taylor Swift', 
+                artist: 'Taylor Swift', 
+                url: 'https://www.amazon.com/Taylor-Swift/dp/B0014I4KH6', 
+                image: 'https://images-na.ssl-images-amazon.com/images/I/61McsadO1OL.jpg', 
+                thumbnail_image: 'https://i.imgur.com/K3KJ3w4h.jpg' 
+            },
+            {
+                title: 'Fearless',
+                artist: 'Taylor Swift',
+                url: 'https://www.amazon.com/Fearless-Enhanced-Taylor-Swift/dp/B001EYGOEM',
+                image: 'https://images-na.ssl-images-amazon.com/images/I/51qmhXWZBxL.jpg',
+                thumbnail_image: 'https://i.imgur.com/K3KJ3w4h.jpg'
+            },
+            {
+                title: 'Speak Now',
+                artist: 'Taylor Swift',
+                url: 'https://www.amazon.com/Speak-Now-Taylor-Swift/dp/B003WTE886',
+                image: 'https://images-na.ssl-images-amazon.com/images/I/51vlGuX7%2BFL.jpg',
+                thumbnail_image: 'https://i.imgur.com/K3KJ3w4h.jpg'
+            },
+            {
+                title: 'Red',
+                artist: 'Taylor Swift',
+                url: 'htts://www.amazon.com/Red-Taylor-Swift/dp/B008XNZMOU',
+                image: 'https://images-na.ssl-images-amazon.com/images/I/41j7-7yboXL.jpg',
+                thumbnail_image: 'https://i.imgur.com/K3KJ3w4h.jpg'
+            },
+            {
+                title: '1989',
+                artist: 'Taylor Swift',
+                url: 'https://www.amazon.com/1989-Taylor-Swift/dp/B00MRHANNI',
+                image: 'https://images-na.ssl-images-amazon.com/images/I/717DWgRftmL._SX522_.jpg',
+                thumbnail_image: 'https://i.imgur.com/K3KJ3w4h.jpg'
+            }
+        ]
+    };
 
     // componentWillMount() {
     //     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
