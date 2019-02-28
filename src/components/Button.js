98f9ef2dbb750aefe39b2fd4f9db158cac9ff26c
andreynho2006 <andreynho2006@gmail.com>
2019-02-28 12:23:47 +0200

    style the button of the Card

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,30 @@ import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
     return (
-        <TouchableOpacity>
-            <Text>Click me!</Text>
+        <TouchableOpacity style={styles.buttonStyle}>
+            <Text style={styles.textStyle}>Click me!</Text>
         </TouchableOpacity>
         
     );
+};
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16, 
+        fontWeight: '600',
+    },
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+    }
 };
 
 export default Button;
